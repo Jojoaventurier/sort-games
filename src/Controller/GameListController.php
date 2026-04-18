6f07906ba875ffe3bf0ac474074262613003cb27
Geoffroy Maurutto <165882140+Jojoaventurier@users.noreply.github.com>
2026-04-18 13:14:24 +0200

    fix

--- src/Controller/GameListController.php
+++ src/Controller/GameListController.php
@@ -12,7 +12,6 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use App\Service\GameMatcher;
 use App\Repository\GameListRepository;
-use App\Repository\GameEntryRepository;
 
 class GameListController extends AbstractController
 {
